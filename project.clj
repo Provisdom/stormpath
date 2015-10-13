e7cc5f1c8194ce90c9d7bad1f7d2a70991180721
Kenny Williams <kennyjwilli@gmail.com>
2015-10-13 14:49:59 -0400

    Add log4j12 to remove warnings in project implementations

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
                  [com.stormpath.sdk/stormpath-sdk-api "1.0.RC4.5"]
                  [com.stormpath.sdk/stormpath-sdk-impl "1.0.RC4.5"]
                  [com.stormpath.sdk/stormpath-sdk-httpclient "1.0.RC4.5"]])
