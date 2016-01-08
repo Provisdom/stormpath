ce366e3fcdbe49c258bcea272ef47c604a04b4fb
Kenny Williams <kennyjwilli@gmail.com>
2016-01-07 18:26:09 -0800

    Update deps (not tested)

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject pro/stormpath "0.1.2"
+(defproject pro/stormpath "0.1.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.slf4j/slf4j-log4j12 "1.7.12"]
-                 [com.stormpath.sdk/stormpath-sdk-api "1.0.RC4.5"]
-                 [com.stormpath.sdk/stormpath-sdk-impl "1.0.RC4.5"]
-                 [com.stormpath.sdk/stormpath-sdk-httpclient "1.0.RC4.5"]])
+                 [com.stormpath.sdk/stormpath-sdk-api "1.0.RC8"]
+                 [com.stormpath.sdk/stormpath-sdk-impl "1.0.RC8"]
+                 [com.stormpath.sdk/stormpath-sdk-httpclient "1.0.RC8"]])
