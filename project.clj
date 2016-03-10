ba63c7fd488efc464791578f53548c60c43d89a2
Kenny Williams <kennyjwilli@gmail.com>
2016-03-10 15:44:54 -0800

    Add stormpath impl jar

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stormpath.sdk/stormpath-sdk-api "1.0.RC8.5"]
                  [com.stormpath.sdk/stormpath-sdk-httpclient "1.0.RC8.5"]
-                 [com.stormpath.sdk/stormpath-sdk-oauth "1.0.RC8.5"]])
+                 [com.stormpath.sdk/stormpath-sdk-oauth "1.0.RC8.5"]
+                 [com.stormpath.sdk/stormpath-sdk-impl "1.0.RC8.5"]])
