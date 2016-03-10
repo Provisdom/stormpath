a72ac29eeb3bdb107cdccdd79121b00efcbc2b7a
Kenny Williams <kennyjwilli@gmail.com>
2016-03-10 15:44:39 -0800

    More test code

--- test/provisdom/stormpath/test/core.clj
+++ test/provisdom/stormpath/test/core.clj
@@ -1,6 +1,7 @@
 (ns provisdom.stormpath.test.core
   (:require [provisdom.stormpath.core :as s]
-            [provisdom.stormpath.oauth :as oauth]))
+            [provisdom.stormpath.oauth :as oauth]
+            [provisdom.stormpath.directory :as dir]))
 
 (defonce client (s/client {:id     (System/getenv "STORMPATH_APP_ID")
                            :secret (System/getenv "STORMPATH_APP_SECRET")}))
@@ -10,4 +11,13 @@
 (comment
   (def jwt-map (oauth/access-token application "john@example.com" "Password1"))
   (oauth/validate-token application (str (:access-token jwt-map)) true)
+
+  (def goog-dir (dir/create-directory client
+                                      {:name        "google-directory"
+                                       :description "a google directory"}
+                                      {:type         :google
+                                       :id           (System/getenv "GOOG_CLIENT_ID")
+                                       :secret       (System/getenv "GOOG_CLIENT_SECRET")
+                                       :redirect-uri "postmessage"}))
+  (def goog-dir (dir/get-directory client "google-directory"))
   )
