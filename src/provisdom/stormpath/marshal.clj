f3cfe0ef3dfa91a2939c091fe00bb3d0c8e01da8
Kenny Williams <kennyjwilli@gmail.com>
2016-03-09 16:25:57 -0800

    Remove old code

--- src/provisdom/stormpath/marshal.clj
+++ src/provisdom/stormpath/marshal.clj
@@ -112,13 +112,3 @@
         (seq? marshalled)
         (map marshal-value marshalled)
         :default marshalled))))
-
-(defmacro obj->map [o & bindings]
-  (let [s (gensym "local")]
-    `(let [~s ~o]
-       ~(->> (partition 2 bindings)
-             (map (fn [[k v]]
-                    (if (vector? v)
-                      [k (list (last v) (list (first v) s))]
-                      [k (list v s)])))
-             (into {})))))
